84080ad3bcd9c25a91d88f93eb7d9721a2802162
Bozhena <bozhena.shalginskaia@mail.ru>
2022-08-18 14:03:30 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="app">
+      <PrimaryForm />
+  </div>
+</template>
+
+<script>
+import PrimaryForm from "@/views/PrimaryForm";
+export default {
+  components: {PrimaryForm}
+}
+</script>
+
+<style>
+html {
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body {
+  margin: 0;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+}
+#app {
+  box-sizing: border-box;
+  width: 500px;
+  padding: 0 20px 20px;
+  margin: 0 auto;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  display: flex;
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+}
+h1 {
+  font-size: 50px;
+  font-weight: 700;
+}
+h2 {
+  font-size: 38px;
+  font-weight: 700;
+}
+h3 {
+  font-size: 28px;
+  font-weight: 700;
+}
+h4 {
+  font-size: 21px;
+  font-weight: 700;
+}
+h5 {
+  font-size: 16px;
+  font-weight: 700;
+}
+h6 {
+  font-size: 15px;
+  font-weight: 700;
+}
+
+
+input,
+textarea {
+  box-sizing: border-box;
+  border: solid 1px rgba(0, 0, 0, 0.4);
+}
+input.error,
+select.error {
+  margin-bottom: 0;
+}
+input + p.errorMessage {
+  margin-bottom: 24px;
+}
+textarea {
+  width: 100%;
+  overflow: auto;
+  font-size: 20px;
+}
+[type="checkbox"],
+[type="radio"] {
+  box-sizing: border-box;
+  padding: 0;
+  margin-right: 0.5rem;
+}
+[type="number"]::-webkit-inner-spin-button,
+[type="number"]::-webkit-outer-spin-button {
+  height: auto;
+}
+[type="search"] {
+  -webkit-appearance: textfield;
+  outline-offset: -2px;
+}
+[type="search"]::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+[type="text"],
+[type="number"],
+[type="search"],
+[type="password"] {
+  height: 52px;
+  width: 100%;
+  padding: 0 10px;
+  font-size: 20px;
+}
+[type="text"]:focus,
+[type="number"]:focus,
+[type="search"]:focus,
+[type="password"]:focus {
+  border-color: #39b982;
+}
+::-webkit-file-upload-button {
+  -webkit-appearance: button;
+  font: inherit;
+}
+[hidden] {
+  display: none;
+}
+.error {
+  border: 1px solid red;
+}
+.field {
+  margin-bottom: 24px;
+}
+</style>
---
+++ src/views/PrimaryForm.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <h1>Заполните форму</h1>
+    <form>
+      <h3>Информация об университете</h3>
+
+      <Input
+          v-model="event.nameOfTheUniversity"
+          label="Название университета"
+          type="text"
+          class="field"
+      />
+      <Input
+          v-model="event.nameOfTheFaculty"
+          label="Название факультета"
+          type="text"
+          class="field"
+      />
+
+      <Input
+          v-model="event.nameOfTheDepartament"
+          label="Специальность"
+          type="text"
+          class="field"
+      />
+
+      <h3>Информация об экзаменаторе</h3>
+
+      <Input
+          v-model="event.TheExaminerLastName"
+          label="Фамилия"
+          type="text"
+          class="field"
+      />
+
+      <Input
+          v-model="event.TheExaminerFirstName"
+          label="Имя"
+          type="text"
+          class="field"
+      />
+
+      <Input
+          v-model="event.TheExaminerMiddleName"
+          label="Отчество"
+          type="text"
+          class="field"
+      />
+
+      <h3>Данные о председателе цикловой комиссии</h3>
+
+      <Input
+          v-model="event.TheChairmanLastName"
+          label="Фамилия"
+          type="text"
+          class="field"
+      />
+
+      <Input
+          v-model="event.TheChairmanFirstName"
+          label="Имя"
+          type="text"
+          class="field"
+      />
+      <Input
+          v-model="event.TheChairmanMiddleName"
+          label="Отчество"
+          type="text"
+          class="field"
+      />
+
+      <h3>Специальность и предмет</h3>
+
+      <Input
+          v-model="event.SpecialtyСode"
+          label="Код специальности"
+          type="number"
+          class="field"
+      />
+      <Input
+          v-model="event.NameOfTheSpecialty"
+          label="Название специальности"
+          type="text"
+          class="field"
+      />
+
+      <Input
+          v-model="event.SubjectName"
+          label="Название предмета"
+          type="text"
+          class="field"
+      />
+
+      <h3>Вопросы</h3>
+
+      <Input
+          v-model="event.theoreticalQuestion"
+          label="Теоретические вопросы"
+          type="text"
+          class="field"
+      />
+      <Input
+          v-model="event.practicalQuestions"
+          label="Практические вопросы"
+          type="text"
+          class="field"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+import Input from "@/components/Input";
+export default {
+  components: {Input},
+  data() {
+    return {
+      event: {
+          nameOfTheUniversity: '',
+          nameOfTheFaculty: '',
+          nameOfTheDepartament:'',
+
+        TheExaminerLastName: '',
+        TheExaminerFirstName: '',
+        TheExaminerMiddleName: '',
+
+        TheChairmanLastName: '',
+        TheChairmanFirstName: '',
+        TheChairmanMiddleName: '',
+
+        SpecialtyСode: '',
+        NameOfTheSpecialty: '',
+
+        SubjectName: '',
+
+        practicalQuestions: [],
+        theoreticalQuestion: []
+
+      }
+      }
+      }
+    }
+</script>
+
+<style scoped>
+</style>
